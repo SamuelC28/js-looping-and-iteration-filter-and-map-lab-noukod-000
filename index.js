a6cdec7bd0fbfc99778883265b99dee775230c04
Samuel Cornet <samuel@noukod.com>
2019-02-04 21:06:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,36 +17,12 @@ function driverNamesWithRevenueOver(drivers, revenue){
     return driversWithRevenueOver(drivers, revenue).map(driver => driver.name);
 }
 
-// function exactMatch(drivers, revenue) {
-//   return drivers.filter(driver => driver, )
-// }
-
-
-//
-// function exactMatch(drivers, {key: value}) {
-//   return drivers.filter(function(driver){
-//   return drivers.filter(attributeFilter);
-// }
-
-
-function exactMatch(drivers, revebue) {
-  if drivers.filter(function (driver)) {
-    return driver.hasOwnProperty(Object.keys(objectSpec)[0] == Object.values(objectSpec)[0];
-  }
+function exactMatch(drivers, objectSpec){
+    return drivers.filter(function (driver) {
+        return driver[Object.keys(objectSpec)[0]] == Object.values(objectSpec)[0];
+    });
 }
 
-
-
-
-
-
-//
-// function exactMatch(drivers, objectSpec){
-//     return drivers.filter(function (driver) {
-//         return driver[Object.keys(objectSpec)[0]] == Object.values(objectSpec)[0];
-//     });
-// }
-//
-// function exactMatchToList(drivers, objectSpec){
-//     return exactMatch(drivers,objectSpec).map(driver => driver.name);
-// }
+function exactMatchToList(drivers, objectSpec){
+    return exactMatch(drivers,objectSpec).map(driver => driver.name);
+}
