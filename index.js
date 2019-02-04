91ecf053569fccb110224471f23dc12583b3dd07
Samuel Cornet <samuel@noukod.com>
2019-02-04 20:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here:
+// // Code your solution here:
+// function findMatching(drivers, word){
+//     return drivers.filter(driver => driver.toLowerCase() == word.toLowerCase());
+// }
+// 
+// function fuzzyMatch(drivers, partialWrold){
+//     return drivers.filter(function (driver){return driver.slice(0, partialWrold.length) == partialWrold});
+// }
+// 
+// function matchName(drivers, word){
+//     return drivers.filter(function (driver){return driver["name"].match(word)});
+// }
+// 
+// ...
+// function driversWithRevenueOver(drivers, revenue){
+//     return drivers.filter(function (driver) {
+//         return driver.revenue > revenue;
+//     });
+// }
+// 
+// function driverNamesWithRevenueOver(drivers, revenue){
+//     return driversWithRevenueOver(drivers,revenue).map(driver => driver.name);
+// }
+// 
+// function exactMatch(drivers, objectSpec){
+//     return drivers.filter(function (driver) {
+//         return driver[Object.keys(objectSpec)[0]] == Object.values(objectSpec)[0];
+//     });
+// }
+// 
+// function exactMatchToList(drivers, objectSpec){
+//     return exactMatch(drivers,objectSpec).map(driver => driver.name);
+// }
