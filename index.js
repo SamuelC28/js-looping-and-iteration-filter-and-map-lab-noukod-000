e4e4d1017e87398198acb7bb57be34457239b11a
Samuel Cornet <samuel@noukod.com>
2019-02-04 20:49:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 
 
 
-function driverNamesWithRevenueOver(drivers, revenue){
-    return driversWithRevenueOver(drivers,revenue).map(driver => driver.name);
-}
+// function driverNamesWithRevenueOver(drivers, revenue){
+//     return driversWithRevenueOver(drivers,revenue).map(driver => driver.name);
+// }
 //
 // function exactMatch(drivers, objectSpec){
 //     return drivers.filter(function (driver) {
