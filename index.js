561387971a65804a6da517f6c45a14394944fc50
Samuel Cornet <samuel@noukod.com>
2019-02-04 21:08:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here:
+// // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver){
+    return parseInt(driver.revenue) > parseInt(revenue)
+  });
+}
+
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver){
+  return driver.revenue > parseInt(revenue)}).map(function(driver) {return driver.name});
+}
+
+
+function driverNamesWithRevenueOver(drivers, revenue){
+    return driversWithRevenueOver(drivers, revenue).map(driver => driver.name);
+}
+
+function exactMatch(drivers, objectSpec){
+    return drivers.filter(function (driver) {
+        return driver[Object.keys(objectSpec)[0]] == Object.values(objectSpec)[0];
+    });
+}
+
+function exactMatchToList(drivers, objectSpec){
+    return exactMatch(drivers,objectSpec).map(driver => driver.name);
+}
